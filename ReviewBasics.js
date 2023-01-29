f443e8f79db631b04d9d70d7a0caadd6a82b5af1
Adrián <123007421+Kirk810@users.noreply.github.com>
2023-01-30 00:24:51 +0100

    Update ReviewBasics.js

--- ReviewBasics.js
+++ ReviewBasics.js
@@ -1,18 +1,20 @@
 //Iteración #1: Mix for e includes
-//Dado el siguiente javascript usa forof para recorrer el array de películas, genera un nuevo array con las categorías de las películas e imprime por consola el array de categorías. Ten en cuenta que las categorías no deberían repetirse. Para filtrar las categorías puedes ayudarte de la función .includes()
+/*Dado el siguiente javascript usa forof para recorrer el array de películas, genera un nuevo array con las categorías de las películas e imprime por 
+consola el array de categorías. Ten en cuenta que las categorías no deberían repetirse. Para filtrar las categorías puedes ayudarte de la función .includes()*/
 const movies = [
     {title: 'Madaraspar', duration: 192, categories: ['comedia', 'aventura']},
     {title: 'Spiderpan', duration: 122, categories: ['aventura', 'acción']},
     {title: 'Solo en Whatsapp', duration: 223, categories: ['comedia', 'thriller']},
     {title: 'El gato con guantes', duration: 111, categories: ['comedia', 'aventura', 'animación']},
 ]
+const categorias = [];
 for(movie of movies) {
-    for(category of categories) {
-        if(!categories.includes(category)) {
-            categories.push(category);
-        }
+    for (category of movies.categories) {
+        if(categorias.includes(category));
+            categorias.push(category)
     }
 }
+console.log(categorias);
 
 //Iteración #2: Mix Fors
 //Dado el siguiente javascript usa forof y forin para hacer la media del volumen de todos los sonidos favoritos que tienen los usuarios.
